967c5e2a63f52f621a86f3ea54915685cd26d442
Zack Brown <zack@zackbrown.co.uk>
2024-10-13 12:14:23 +0100

    Add viewer example app

---
+++ Proscenium Viewer/Proscenium Viewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//
+//  Created by Zack Brown on 13/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Proscenium Viewer/Proscenium Viewer/Proscenium_ViewerApp.swift
@@ -0,0 +1,19 @@
+//
+//  Proscenium_ViewerApp.swift
+//
+//  Created by Zack Brown on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Proscenium_ViewerApp: App {
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
